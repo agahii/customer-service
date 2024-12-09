048c989251755c93f4ec7ed72fb55bd7fa8aed78
Shaheer Hasan <syedshaheerhasan1@gmail.com>
2024-12-09 18:44:18 +0500

    dsd

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1 style={{ color: "#333" }}>Simple Home Page</h1>
+      <h1 style={{ color: "#333" }}>Simple Home page</h1>
       <p style={{ color: "#333" }}>Your paragraph goes here...</p>
     </div>
   );
